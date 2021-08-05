ded9d90a8e9ab007ca23b0fc1e41879bf2d3ad44
gregrolwes <gregory.rolwes@slu.edu>
2021-08-05 17:51:39 -0500

    Added quote rotate functionality

--- src/app/components/quote-carousel/quote-carousel.component.ts
+++ src/app/components/quote-carousel/quote-carousel.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quotes from './quotes.json';
 
 @Component({
   selector: 'app-quote-carousel',
@@ -6,7 +7,18 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./quote-carousel.component.css'],
 })
 export class QuoteCarouselComponent implements OnInit {
+  currentQuotes: any[] = quotes['Network states'];
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  shiftRight(): void {
+    // shift order of quote cards to the right, the last card will become the first card
+    this.currentQuotes.unshift(this.currentQuotes.pop());
+  }
+  shiftLeft(): void {
+    // shift order of quote cards to the left, the first card will become the last card
+    this.currentQuotes.push(this.currentQuotes.shift());
+  }
 }

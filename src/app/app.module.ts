4392e995eab7899f4dd17eca418caaf00170d299
gregrolwes <gregory.rolwes@slu.edu>
2021-08-04 21:54:36 -0500

    Added feature columns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PrimaryCtaComponent } from './components/primary-cta/primary-cta.component';
 import { CtaButtonComponent } from './components/cta-button/cta-button.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { FeatureColumnComponent } from './components/feature-column/feature-column.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimaryCtaComponent,
-    CtaButtonComponent
+    CtaButtonComponent,
+    FeaturesComponent,
+    FeatureColumnComponent
   ],
   imports: [
     BrowserModule
